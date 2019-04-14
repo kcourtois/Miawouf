fbb2a012f0f1a7b7e267d14081320bc3fe82c59b
kcourtois <courtois.k2@gmail.com>
2019-04-15 00:23:33 +0200

    setup of xcode project with its assets

---
+++ Miawouf/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Miawouf
+//
+//  Created by Kévin Courtois on 14/04/2019.
+//  Copyright © 2019 Kévin Courtois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
